a6c499525ea6fdca0616115df6c4eb90ed6e70e9
Deven <dmaheshwari20@stuy.edu>
2022-05-17 15:35:46 -0400

    Created KSA and RGA functions from pseudocode.

--- encoder.py
+++ encoder.py
@@ -3,25 +3,43 @@
 # Final Project - RC4
 # May 31, 2022
 
-def KSA():
-    S = []
-    for i in range(255:)
-        S[i] := i
-endfor
-j := 0
-for i from 0 to 255
-    j := (j + S[i] + key[i mod keylength]) mod 256
-    swap values of S[i] and S[j]
-endfor
+def swap(one, two): 
+    temp = one
+    one = two
+    two = temp
 
-def RGA():
-    i := 0
-j := 0
-while GeneratingOutput:
-    i := (i + 1) mod 256
-    j := (j + S[i]) mod 256
-    swap values of S[i] and S[j]
-    K := S[(S[i] + S[j]) mod 256]
-    output K
-endwhile
+def KSA(key):
+    """
+    KSA 
+        Arguments:
+        Algorithm: 
+        Return:
+    """
+
+    S = []
+    for i in range(255):
+        S[i] = i
+    j = 0
+    for i in range(255): 
+        j = (j + S[i] + key[i & len(key)]) % 256
+        swap(S[i], S[j])
+
+def RGA(iterate):
+    """
+    RGA
+        Arguments:
+        Algorithm: 
+        Return:
+    """
+
+    i = 0
+    j = 0
+    index = 0
+    while index <= iterate:
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        swap(S[i], S[j])
+        K = S[(S[i] + S[j]) % 256]
+        return K
+        index += 1
 
