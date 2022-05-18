80656bfe1cf4a35d803009931ead9b0692492a31
Deven <dmaheshwari20@stuy.edu>
2022-05-18 14:42:21 -0400

    Added sources tab readme, fixed bit reading enoder

--- encoder.py
+++ encoder.py
@@ -3,6 +3,8 @@
 # Final Project - RC4
 # May 31, 2022
 
+import numpy as np
+
 def swap(one, two): 
     temp = one
     one = two
@@ -16,15 +18,17 @@ def KSA(key):
         Return:
     """
 
-    S = []
-    for i in range(255):
-        S[i] = i
+    S = [i for i in range(256)]
+    key_array = bytearray(key, 'utf-8')
     j = 0
-    for i in range(255): 
-        j = (j + S[i] + key[i & len(key)]) % 256
-        swap(S[i], S[j])
+    for i in range(256): 
+        j = (j + S[i] + key_array[i % len(key_array)]) % 256
+        temp = S[i]
+        S[i] = S[j]
+        S[j] = temp
+    return S
 
-def RGA(iterate):
+def RGA(iterate, arr):
     """
     RGA
         Arguments:
@@ -37,9 +41,19 @@ def RGA(iterate):
     index = 0
     while index <= iterate:
         i = (i + 1) % 256
-        j = (j + S[i]) % 256
-        swap(S[i], S[j])
-        K = S[(S[i] + S[j]) % 256]
-        return K
+        j = (j + arr[i]) % 256
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+        k = arr[(arr[i] + arr[j]) % 256]
         index += 1
+        return k
 
+def main():
+    test = (KSA("test1"))
+    print(test)
+    print('-----------------------------------')
+    print(RGA(5, test))
+
+if __name__ == "__main__":
+    main()
