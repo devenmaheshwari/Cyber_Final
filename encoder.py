84e00de8b4ad3ea8ca51c67c91677f9a2043ddeb
J3rry-L <jliang21@stuy.edu>
2022-05-24 15:15:27 -0400

    Deleted extraneous code

--- encoder.py
+++ encoder.py
@@ -114,7 +114,6 @@ def file_encoder(plaintext_file, key_file, output_name):
             output_name - file to be written to with ciphertext
         Algorithm:
             Uses KSA and RGA and XOR's the plaintext with the generated array to produce the ciphertext.
-
     """
 
     plaintext = open(plaintext_file, "rb")
@@ -131,13 +130,13 @@ def file_encoder(plaintext_file, key_file, output_name):
     output_file.close()
 
 def main():
-    test = KSA("key")
+    test = KSA("Key")
     print(test)
     print()
     test2 = RGA(len(test), test)
     print(test2)
     print()
-    print(encoder('plaintext', "key"))
+    print(encoder('Plaintext', "Key"))
     file_encoder("plaintext.txt", "key.txt", "ciphertext.txt")
 
 if __name__ == "__main__":
