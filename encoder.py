b68d5ce16f47f4ea86b5d0f975ab41bd21734064
Jerry Liang <jliang21@stuy.edu>
2022-05-23 23:33:48 -0400

    Fixed encoder to work with files; created helper KSA_byte method. Output matches expected hex value.

--- encoder.py
+++ encoder.py
@@ -12,18 +12,40 @@ import numpy as np
 
 def KSA(key):
     """
-    KSA - Key Scheduling Algorithm in order to create an array of byte values to be used in RGA for encoding. 
+    KSA - Key Scheduling Algorithm in order to create an array of byte values to be used in RGA for encoding.
         Arguments:
             key - string
         Algorithm:
             Initializes an array of length 256 to represent the number of bytes available for key encoding then
             switches values within the array using the key length and key length in bytes.
         Return:
-            S - byte array 
+            S - byte array
     """
 
     S = [i for i in range(256)]
     key_array = bytearray(key, 'utf-8')
+    j = 0
+    for i in range(256):
+        j = (j + S[i] + key_array[i % len(key_array)]) % 256
+        temp = S[i]
+        S[i] = S[j]
+        S[j] = temp
+    return S
+
+def KSA_byte(key):
+    """
+    KSA - Key Scheduling Algorithm in order to create an array of byte values to be used in RGA for encoding.
+        Arguments:
+            key - bytes
+        Algorithm:
+            Initializes an array of length 256 to represent the number of bytes available for key encoding then
+            switches values within the array using the key length and key length in bytes.
+        Return:
+            S - byte array
+    """
+
+    S = [i for i in range(256)]
+    key_array = bytearray(key)
     j = 0
     for i in range(256):
         j = (j + S[i] + key_array[i % len(key_array)]) % 256
@@ -41,10 +63,10 @@ def RGA(iterate, arr):
             iterate - number of values to be generated in the bit array
             arr - the identity permutation from the KSA
         Algorithm:
-            For iternates number of times, adds S[i] to j and swaps S[i] with S[j] and uses a separate value 
-            from S fm the keystream. 
+            For iternates number of times, adds S[i] to j and swaps S[i] with S[j] and uses a separate value
+            from S fm the keystream.
         Return:
-            answer - bit array to be XOR'ed     
+            answer - bit array to be XOR'ed
     """
 
     i = 0
@@ -73,7 +95,7 @@ def encoder(plaintext, key):
             plaintext - string
             key - string
         Algorithm:
-            Uses KSA and RGA and XOR's the plaintext with the generated array to produce the ciphertext. 
+            Uses KSA and RGA and XOR's the plaintext with the generated array to produce the ciphertext.
         Return:
             ciphertext - bit array
     """
@@ -96,7 +118,7 @@ def file_encoder(plaintext_file, key_file, output_name):
             key_file - file
             output_name - file to be written to with ciphertext
         Algorithm:
-            Uses KSA and RGA and XOR's the plaintext with the generated array to produce the ciphertext. 
+            Uses KSA and RGA and XOR's the plaintext with the generated array to produce the ciphertext.
 
     """
 
@@ -105,10 +127,10 @@ def file_encoder(plaintext_file, key_file, output_name):
     output_file = open(output_name, "wb")
 
     plaintext_array = bytearray(plaintext.read())
-    keystream = RGA(len(plaintext_array), KSA(key.read()))
-    
+    keystream = RGA(len(plaintext_array), KSA_byte(key.read()))
+
     for i in range(len(plaintext_array)):
-        output_file.write(plaintext_array[i] ^ keystream[i])
+        output_file.write((plaintext_array[i] ^ keystream[i]).to_bytes(1, "big"))
     plaintext.close()
     key.close()
     output_file.close()
@@ -121,6 +143,7 @@ def main():
     print(test2)
     print()
     print(encoder('plaintext', "key"))
+    file_encoder("plaintext.txt", "key.txt", "ciphertext.txt")
 
 if __name__ == "__main__":
     main()
